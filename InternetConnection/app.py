7b03a8388b6f19970815b7e573c69930edb6f074
QiFuChina <110169005+fuqi0714@users.noreply.github.com>
2023-12-04 13:33:05 +0800

    Update Server and Html

--- InternetConnection/app.py
+++ InternetConnection/app.py
@@ -13,8 +13,15 @@ def upload():
         basepath = os.path.dirname(__file__)  # 当前文件所在路径
         upload_path = os.path.join(basepath, r'static\uploads',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
-        return redirect(url_for('upload'))
+        return redirect(url_for('success'))
     return render_template('upload.html')
 
+
+@app.route('/success', methods=['POST', 'GET'])
+def success():
+
+    return render_template('success.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='172.29.84.103',port=5000)
