addb7646aa637382bdca1e12f78829f1a38f36dc
fuqi0714 <night7phoenix@gmail.com>
2023-12-04 14:36:59 +0800

    Update Start Page

--- InternetConnection/app.py
+++ InternetConnection/app.py
@@ -4,14 +4,30 @@ from flask import Flask,render_template,request,redirect,url_for
 from werkzeug.utils import secure_filename
 import os
 
+download_path=r'D:\fq\2023-2024-1\05A21061\Projects'
+
 app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def start():
+    if request.method == 'POST':
+        print("start→upload")
+        return redirect(url_for('upload'))
+    return render_template('start.html')
+
+@app.route('/download', methods=['POST', 'GET'])
+def download():
+    if request.method == 'GET':
+        
+        return send_from_directory(download_path) 
+    return send_from_directory(download_path) 
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     if request.method == 'POST':
         f = request.files['file']
         basepath = os.path.dirname(__file__)  # 当前文件所在路径
-        upload_path = os.path.join(basepath, r'static\uploads',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        upload_path = os.path.join(basepath, r'D:\fq\2023-2024-1\05A21061\Projects',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
         return redirect(url_for('success'))
     return render_template('upload.html')
@@ -24,4 +40,4 @@ def success():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='172.29.84.103',port=5000)
+    app.run(debug=True,host='172.30.5.24',port=5000)
